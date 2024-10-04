2c76bc1b3e94b2be311d0e3d8e92aab19d369e7a
Joshim <md.joshimu01@gmail.com>
2024-10-04 07:42:34 +0600

    composer.lock was missing

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp-lover/blog-writer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5843848fd9cdefe18ef67a3ed1421793cf1dacc3',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wp-lover/blog-writer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5843848fd9cdefe18ef67a3ed1421793cf1dacc3',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
