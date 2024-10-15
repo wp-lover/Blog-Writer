780bdcd57a7f1566c641d8ebb39d4c90c12bd633
Joshim <md.joshimu01@gmail.com>
2024-10-15 08:57:47 +0600

    rewriting stylesForBlocks.php

--- Inc/Blocks/StylesForBlocks.php
+++ Inc/Blocks/StylesForBlocks.php
@@ -1,3 +1,4 @@
+
 <?php
 
 namespace WpLover\BlogWriter\Inc\Blocks;
@@ -8,6 +9,8 @@ class StylesForBlocks
     private static $instance = null;
 
     public $headerAttributes = [];
+
+    public $sidebarAttributes = [];
 
     private function __construct()
     {
@@ -32,6 +35,51 @@ class StylesForBlocks
     function styles( )
     {
        $this->headerStyles();
+
+       $this->sidebarMenuStype();
+    }
+
+    function sidebarMenuStype()
+    {   
+        if ( empty( $this->sidebarAttributes )  ) {
+
+            return;
+        }
+
+        $is_hidden_desktop = false;
+        
+        if ( isset( $this->sidebarAttributes['is_hidden_desktop'] ) && $this->sidebarAttributes['is_hidden_desktop'] ) {
+            $is_hidden_desktop = true;
+        }
+        
+        ?><style>
+            @media ( min-width : 767px ) {
+                #gsp-sidebar-menu-icon {
+                    display: <?php echo  $is_hidden_desktop == true ? 'none' : 'block' ?>;
+                }
+            }
+            #gsp-sidebar-container{
+                margin-top: <?php print_value($this->sidebarAttributes['container_margin_t'])  ?>px;
+            }
+            #gsp-sidebar-inside{
+                background-color: <?php print_value($this->sidebarAttributes['container_bg_color'])  ?>;
+            }
+            #gsp-sidebar-container li{
+                background-color: <?php print_value($this->sidebarAttributes['item_bg_color'])  ?>;
+            }
+            #gsp-sidebar-container li:hover {
+                background-color: <?php print_value($this->sidebarAttributes['item_bg_hover_color'])  ?>;
+            }
+            #gsp-sidebar-container li a {
+                color: <?php print_value($this->sidebarAttributes['item_text_color'])  ?>;
+            }
+            #gsp-sidebar-container li:hover a {
+                color: <?php print_value($this->sidebarAttributes['item_text_hover_color'])  ?>;
+            }
+            #gsp-sidebar-container li a path {
+                fill: <?php print_value($this->sidebarAttributes['item_text_color'])  ?>;
+            }
+        </style><?php
     }
 
     function headerStyles()
@@ -39,10 +87,10 @@ class StylesForBlocks
         if ( ! empty( $this->headerAttributes ) ) {
             
             ?><style> 
-            #gsp-header-menu{
-               margin-bottom: -30px;
-            }
-.nav-item{
+#gsp-header-menu{
+    margin-bottom: <?php print_value($this->headerAttributes['menu_container_margin_y'])  ?>px;
+}
+#gsp-header-menu .nav-item{
     min-width: <?php print_value($this->headerAttributes['item_min_width'])  ?>px;
     background-color: <?php print_value($this->headerAttributes['item_bg_color'])  ?>;
     margin-top: <?php print_value( $this->headerAttributes['item_margin_t'] ); ?>px;
@@ -55,46 +103,55 @@ class StylesForBlocks
     border-bottom-left-radius: <?php echo ( $this->headerAttributes['item_margin_b'] ) ? '8px' : '0px'  ?>;
     border-bottom-right-radius: <?php echo ( $this->headerAttributes['item_margin_b'] ) ? '8px' : '0px'  ?>;
 }
-
-.item-parent ul li{
+#gsp-header-menu .item-parent{
+    cursor: pointer;
+}
+#gsp-header-menu .item-parent ul li{
     margin-top: 5px;
 }
 
-.nav-item :hover {
+#gsp-header-menu .nav-item:hover {
     background-color: <?php print_value($this->headerAttributes['item_bg_hover_color'])  ?>;
-    color: <?php print_value($this->headerAttributes['item_text_hover_color'])  ?>;
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
+    padding: 0px;
     border-bottom-left-radius: <?php echo ( $this->headerAttributes['item_margin_b'] ) ? '8px' : '0px'  ?>;
     border-bottom-right-radius: <?php echo ( $this->headerAttributes['item_margin_b'] ) ? '8px' : '0px'  ?>;
 }
 
-.nav-item a {
+#gsp-header-menu .nav-item:hover a {
+    display: flex;
+    color: <?php print_value($this->headerAttributes['item_text_hover_color'])  ?>;
+    
+}
+
+#gsp-header-menu .nav-item a {
+    display: flex;
+    color: <?php print_value( $this->headerAttributes['item_text_color'] );?>;
+    text-decoration: none;
+    border: none;
     padding-top: <?php print_value( $this->headerAttributes['item_padding_t'] ); ?>px;
     padding-left: <?php print_value( $this->headerAttributes['item_padding_l'] ); ?>px;
     padding-bottom: <?php print_value( $this->headerAttributes['item_padding_b'] ); ?>px;
     padding-right: <?php print_value( $this->headerAttributes['item_padding_r'] ); ?>px;
+    margin: 0px;
 }
-.nav-item svg path {
+#gsp-header-menu .nav-item svg path {
     fill: <?php print_value( $this->headerAttributes['item_text_color'] ); ?>;
 }
 
-.nav-item a {
-    color: <?php print_value( $this->headerAttributes['item_text_color'] );?>;
-    text-decoration: none; border: none;
-    margin: 0;
-}
 
-.item-parent .sub-menu{
+#gsp-header-menu .item-parent .sub-menu{
     background-color: transparent;
     margin-top: <?php print_value( $this->headerAttributes['sub_menu_margin_t']  );  ?>px;
+    z-index: 800;
     
 }
-.item-parent .sub-menu li {
+#gsp-header-menu .item-parent .sub-menu li {
     margin-bottom: <?php print_value( $this->headerAttributes['child_item_margin_b']  );  ?>px;
     border-radius: 10px;
 }
-.item-parent .sub-menu li :hover{
+#gsp-header-menu .item-parent .sub-menu li :hover{
     border-radius: 10px;
 }
 
@@ -109,55 +166,88 @@ class StylesForBlocks
     { 
         ?>
         <script>
-            function gsp_header_sub_menu(itemId , marginTop){
+            document.addEventListener( 'DOMContentLoaded' , function () {
 
-            var item = document.getElementById( 'nav-item-' + itemId )
+            const gspHeaderMenu = document.getElementById('gsp-header-menu');
+
+            var gspHeaderItemParents  = gspHeaderMenu.getElementsByClassName('item-parent');
+
+            document.addEventListener( 'click', function (e) {
+
+                
+                for (let i = 0; i < gspHeaderItemParents.length; i++) {
+
+                    const element = gspHeaderItemParents[i];
+
+                    if (e.target.tagName == 'A') {
+                        const li = e.target.parentNode;
+
+                        if (li == element) {
+                            gsp_header_sub_menu(element);
+                        } 
+                    }
+
+                    if ( e.target.tagName == 'SPAN') {
+                        const aTag = e.target.parentNode;
+
+                        if (aTag.parentNode == element) {
+
+                            gsp_header_sub_menu(element);
+                        }
+                    }
+                }
+            });
     
-            var child = item.getElementsByClassName( 'sub-menu' );
+    // console.log(gspHeaderItemParents);
 
-            // if not container class named 'hidden'
-            if (!child[0].classList.contains('hidden') ) {
+  function gsp_header_sub_menu( item ){
 
-                child[0].style.opacity = 0;
-                setTimeout( function(){
-                    child[0].classList.remove( 'd-block' );
-                    child[0].classList.add( 'hidden' );
-                    
-                } , 450 );  
-                
-                controll_dropdown_icon( itemId , "<?php print_value( $this->headerAttributes['item_text_color'] );?>", false );
+         var child = item.getElementsByClassName( 'sub-menu' );
 
-            }else{
-                
+         // if not container class named 'hidden'
+         if (!child[0].classList.contains('hidden') ) {
 
+             child[0].style.opacity = 0;
+             setTimeout( function(){
+                 child[0].classList.remove( 'd-block' );
+                 child[0].classList.add( 'hidden' );
+                 
+             } , 450 );  
+             
+             controll_dropdown_icon( item , "<?php print_value( $this->headerAttributes['item_text_color'] );?>", false );
 
-                child[0].classList.remove( 'hidden' );
-                child[0].classList.add( 'd-block' );
-
-                
-                setTimeout( function(){
-                    child[0].style.opacity = 1;
-                } , 100 );
-
-                
-                controll_dropdown_icon( itemId , "<?php print_value( $this->headerAttributes['item_text_color'] );?>" , true );
-            }     
-}
-
-function controll_dropdown_icon(itemId , fillColor , isOpen )
-{
-    let path = document.getElementById( "path-id-" + itemId );
+         }else{
+             
 
 
-    if (isOpen) {
-        path.setAttribute( "d" , "M6 15l6-6 6 6" ); 
-        path.setAttribute( "fill" , fillColor ); 
-    }else{
-        path.setAttribute( "d" , "M6 9l6 6 6-6" ); 
-        path.setAttribute( "fill" , fillColor ); 
-    }
+             child[0].classList.remove( 'hidden' );
+             child[0].classList.add( 'd-block' );
+
+             
+             setTimeout( function(){
+                 child[0].style.opacity = 1;
+             } , 100 );
+
+             
+             controll_dropdown_icon( item , "<?php print_value( $this->headerAttributes['item_text_color'] );?>" , true );
+         }     
+ }
+
+     function controll_dropdown_icon(item , fillColor , isOpen )
+     {
+        let path = item.querySelector('path');
+
+
+        if (isOpen) {
+            path.setAttribute( "d" , "M6 15l6-6 6 6" ); 
+            path.setAttribute( "fill" , fillColor ); 
+        }else{
+            path.setAttribute( "d" , "M6 9l6 6 6-6" ); 
+            path.setAttribute( "fill" , fillColor ); 
+        }
      
-}
+     }
+});
         </script>
         <?php
 
